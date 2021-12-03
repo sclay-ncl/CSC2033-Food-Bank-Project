c6cbb094889dd223a72ad6e1a008758669966707
Anthony <c0033688@newcaste.ac.uk>
2021-12-03 11:32:55 +0000

    Fixed database connection issue, everyone must ensure local port is set to 8989.

--- app.py
+++ app.py
@@ -1,25 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from sshtunnel import SSHTunnelForwarder
-import pymysql
 
 app = Flask(__name__)
 
-# ssh tunnel set up, untested until ssh servers are back up
-
-# tunnel = SSHTunnelForwarder(
-#     ssh_address_or_host='linux.cs.ncl.ac.uk',
-#     ssh_username="username",
-#     ssh_password='password',
-#     remote_bind_address=('cs-db.ncl.ac.uk', 3306)
-# )
-
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = \
-#     'mysql+pymysql:///csc2033_team15:Pea5NudeCure@localhost:{}/csc2033_team15'.format(tunnel.local_bind_port)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///temp.db'  # temporary db hosted locally to test db model while uni ssh servers are down
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+     'mariadb+mariadbconnector://csc2033_team15:Pea5NudeCure@127.0.0.1:8989/csc2033_team15'
 
 db = SQLAlchemy(app)
 
