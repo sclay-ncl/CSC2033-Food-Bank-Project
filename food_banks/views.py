b4cc29a7bc9313c4d79df3f8643176f7b8f3b06e
Anthony <c0033688@newcaste.ac.uk>
2021-12-13 11:50:56 +0000

    Add app route for food bank info and search.

--- food_banks/views.py
+++ food_banks/views.py
@@ -4,9 +4,15 @@ from flask import redirect, url_for, render_template, flash, Blueprint, session
 food_banks_blueprint = Blueprint('food_banks', __name__, template_folder='templates')
 
 
-@food_banks_blueprint.route('/food_banks')
-def food_banks():
+@food_banks_blueprint.route('/food_banks_search')
+def food_banks_search():
     latitude = 54.988620
     longitude = -1.598900
 
     return render_template('food-bank-search.html', latitude=latitude, longitude=longitude)
+
+
+@food_banks_blueprint.route("/food_banks_info")
+def food_banks_info():
+
+    return render_template('food-bank-information.html')
