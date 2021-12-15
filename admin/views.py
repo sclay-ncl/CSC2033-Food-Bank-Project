4b97dc9b9dbf8d3c73e2cb95f4c114eb027ace57
sol <s.clay2@newcastle.ac.uk>
2021-12-15 17:24:24 +0000

    Fix indention error

--- admin/views.py
+++ admin/views.py
@@ -5,8 +5,8 @@ from admin.forms import FoodBankRegistrationForm
 from models import User, FoodBank
 from werkzeug.security import check_password_hash, generate_password_hash
 
-
 admin_blueprint = Blueprint('admin', __name__, template_folder='templates')
+
 
 @login_required
 @requires_roles('admin')
@@ -14,30 +14,31 @@ admin_blueprint = Blueprint('admin', __name__, template_folder='templates')
 def admin():
     return render_template('admin.html')
 
+
 @login_required
 @requires_roles('admin')
 @admin_blueprint.route('/food-bank-registration', methods=['GET', 'POST'])
 def food_bank_registration():
     form = FoodBankRegistrationForm()
 
-    if form.validate_on_submit(): # check if email is already used in the database
+    if form.validate_on_submit():  # check if email is already used in the database
         user = User.query.filter_by(email=form.email.data).first()
 
         if user:
             flash("This email is already in use")
             return render_template('food-bank-registration.html', form=form)
 
-    new_food_bank = FoodBank(name=form.name.data,
-                             email=form.email.data,
-                             phone_number=form.phone_number.data,
-                             website=form.website.data)
+        new_food_bank = FoodBank(name=form.name.data,
+                                 email=form.email.data,
+                                 phone_number=form.phone_number.data,
+                                 website=form.website.data)
 
-    new_user = User(email=form.email.data,  # creates user to manage the food bank
-                    first_name=form.name.data,
-                    last_name="N/A",
-                    password=form.password.data,  # TODO: hash password when hash function is working
-                    phone_number=form.phone_number.data,
-                    role='food_bank')
+        new_user = User(email=form.email.data,  # creates user to manage the food bank
+                        first_name=form.name.data,
+                        last_name="N/A",
+                        password=form.password.data,  # TODO: hash password when hash function is working
+                        phone_number=form.phone_number.data,
+                        role='food_bank')
 
     new_food_bank.associated.append(new_user)  # associates the user account with the food bank
     db.session.add(new_food_bank)
