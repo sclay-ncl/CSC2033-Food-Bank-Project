cc235a4631ec0b05222c84dafbae94721fcd28a3
c0053303 <t.goulandris1@newcastle.ac.uk>
2021-12-15 16:13:49 +0000

    Add a food bank registration page that allows admins to add food bank details

--- admin/forms.py
+++ admin/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField, SelectField
-from wtforms.validators import Email, Length, InputRequired
+from wtforms.validators import Email, Length, InputRequired, EqualTo
 
 
 class FoodBankRegistrationForm(FlaskForm):
@@ -13,12 +13,15 @@ class FoodBankRegistrationForm(FlaskForm):
     website = StringField(validators=[Length(max=100)])
     password = PasswordField(validators=[InputRequired(), Length(min=8, max=16, message='Password must be between 8 '
                                                                                         'and 16 characters.')])
+    confirm_password = PasswordField(
+        validators=[InputRequired(), EqualTo('password', message='This must be the same as '
+                                                                 'the password.')])
 
     # address information
     building_name = StringField(validators=[Length(max=100)])  # not all places have a building name, therefore nullable
     number_and_road = StringField(validators=[InputRequired(), Length(max=50)])
     town = StringField(validators=[InputRequired(), Length(max=50)])
-    post_code = StringField(validators=[InputRequired(), Length(max=8)])  # TODO: Add postcode validation
+    postcode = StringField(validators=[InputRequired(), Length(max=8)])  # TODO: Add postcode validation
 
     submit = SubmitField()
 
