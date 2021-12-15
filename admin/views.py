beb5520a9dc8cd9883cc9f851280a11976e32e30
sol <s.clay2@newcastle.ac.uk>
2021-12-15 17:06:28 +0000

    Update food_bank_register function actually register a food bank

--- admin/views.py
+++ admin/views.py
@@ -1,7 +1,10 @@
 from flask_login import current_user, login_user, logout_user, login_required
 from flask import redirect, url_for, render_template, flash, Blueprint, session
-from app import requires_roles
+from app import requires_roles, db
 from admin.forms import FoodBankRegistrationForm
+from models import User, FoodBank
+from werkzeug.security import check_password_hash, generate_password_hash
+
 
 admin_blueprint = Blueprint('admin', __name__, template_folder='templates')
 
@@ -16,4 +19,28 @@ def admin():
 @admin_blueprint.route('/food-bank-registration')
 def food_bank_registration():
     form = FoodBankRegistrationForm()
+
+    if form.validate_on_submit(): # check if email is already used in the database
+        user = User.query.filter_by(email=form.email.data).first()
+
+        if user:
+            flash("This email is already in use")
+            return render_template('food-bank-registration.html', form=form)
+
+    new_food_bank = FoodBank(name=form.name.data,
+                             email=form.email.data,
+                             phone_number=form.phone_number.data,
+                             website=form.website.data)
+
+    new_user = User(email=form.email.data,  # creates user to manage the food bank
+                    first_name=form.name.data,
+                    last_name="N/A",
+                    password=form.password.data,  # TODO: hash password when hash function is working
+                    phone_number=form.phone_number.data,
+                    role='food_bank')
+
+    new_food_bank.associated.append(new_user)  # associates the user account with the food bank
+    db.session.add(new_food_bank)
+    db.session.add(new_user)
+
     return render_template('food-bank-registration.html', form=form)
