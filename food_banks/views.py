bf2218046f8d52657bc0db02ff5d9decac8d92bc
sol <s.clay2@newcastle.ac.uk>
2021-12-15 16:38:12 +0000

    Add update information view for food bank views

--- food_banks/views.py
+++ food_banks/views.py
@@ -1,17 +1,24 @@
 from flask_login import current_user, login_user, logout_user, login_required
 from flask import redirect, url_for, render_template, flash, Blueprint, session
 from app import requires_roles
+from food_banks.forms import UpdateFoodBankInformationForm
 
 food_banks_blueprint = Blueprint('food_banks', __name__, template_folder='templates')
 
 @login_required
 @requires_roles('food_bank')
-@food_banks_blueprint.route('/manage_stock')
+@food_banks_blueprint.route('/manage-stock')
 def manage_stock():
     return render_template('manage-stock.html')
 
 @login_required
 @requires_roles('food_bank')
-@food_banks_blueprint.route('/upcoming_appointments')
+@food_banks_blueprint.route('/upcoming-appointments')
 def upcoming_appointments():
     return render_template('upcoming-appointments.html')
+
+@login_required
+@food_banks_blueprint.route('/update-food-bank-profile')
+def update_information():
+    form = UpdateFoodBankInformationForm()
+    return render_template('update-food-bank-profile.html', form=form)
