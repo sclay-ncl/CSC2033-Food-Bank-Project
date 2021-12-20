4f6a6e6b2b7b888e33a8c1157d83f426d5986d04
sol <s.clay2@newcastle.ac.uk>
2021-12-20 12:40:11 +0000

    Add update_information view to food_bank views

--- food_banks/views.py
+++ food_banks/views.py
@@ -18,8 +18,19 @@ def upcoming_appointments():
     return render_template('upcoming-appointments.html')
 
 @login_required
-@food_banks_blueprint.route('/update-food-bank-profile')
+@requires_roles('food_bank')
+@food_banks_blueprint.route('/update-food-bank-profile', methods=['POST', 'GET'])
 def update_information():
     form = UpdateFoodBankInformationForm()
-    # TODO: actually use form data
+    food_bank = current_user.assosiated[0]  # get food bank associated with user
+    if form.validate_on_submit():  # if form is valid
+        food_bank.update_information(name=form.name.data, email=form.email.data, phone_number=form.phone_number.data,
+                                     website=form.website.data)
+        return update_information()
+
+    # get original food bank details and load them into the form
+    form.name.data = food_bank.name
+    form.email.data = food_bank.email
+    form.phone_number.data = food_bank.phone_number
+    form.website.data = food_bank.website
     return render_template('update-food-bank-profile.html', form=form)
