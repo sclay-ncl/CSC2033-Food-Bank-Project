79d9de9b4305cfaad7957c6968479054ea2defb2
Anthony <c0033688@newcaste.ac.uk>
2022-01-03 15:12:55 +0000

    Added error handlers links, html code for error pages and css

--- app.py
+++ app.py
@@ -38,6 +38,27 @@ def index():  # put application's code here
     return render_template('index.html')  # TODO: create index.html and render it (front end)
 
 
+# ERROR PAGE VIEWS
+@app.errorhandler(400)
+def bad_request(error):
+    return render_template('400.html'), 400
+
+
+@app.errorhandler(403)
+def page_forbidden(error):
+    return render_template('403.html'), 403
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(503)
+def internal_error(error):
+    return render_template('503.html'), 503
+
+
 if __name__ == '__main__':
     my_host = "127.0.0.1"
     free_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -66,4 +87,4 @@ if __name__ == '__main__':
     app.register_blueprint(admin_blueprint)
     app.register_blueprint(food_banks_blueprint)
 
-    app.run(debug=True)
+    app.run(host=my_host, port=free_port, debug=True)
