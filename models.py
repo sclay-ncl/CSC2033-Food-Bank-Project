2dcdd9c31ed6d0e784a1769c1d4edb4f72fbfca4
Anthony <c0033688@newcaste.ac.uk>
2022-01-07 16:16:02 +0000

    Added methods to generate time based auth key and verify it

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
-from app import db
+from app import db, app
 from flask_login import UserMixin
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
 
 class User(db.Model, UserMixin):
@@ -33,6 +34,19 @@ class User(db.Model, UserMixin):
         self.town = town
         self.postcode = postcode
         db.session.commit()
+
+    def get_reset_token(self, expires_sec=900):
+        s = Serializer(app.config['SECRET_KEY'], expires_sec)
+        return s.dumps({'user_id': self.id}).decode('utf-8')
+
+    @staticmethod
+    def verify_reset_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token)['user_id']
+        except:
+            return None
+        return User.query.get(user_id)
 
 
 class FoodBank(db.Model):
