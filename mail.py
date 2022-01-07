77176fa1342ad076219c77342d5764dccd21846e
Anthony <c0033688@newcaste.ac.uk>
2022-01-07 16:15:30 +0000

    Added mail function for sending password reset emails. *Still needs to be worked on*

--- mail.py
+++ mail.py
@@ -3,6 +3,7 @@ import os
 import smtplib
 from email.message import EmailMessage
 from models import FoodBank, User
+from flask import url_for
 
 
 def send_mail():
@@ -32,3 +33,35 @@ def send_mail():
         # smtp.login('noreply.feedingnewcastle@gmail.com', 'wfrdqalctmuzgcet')
         smtp.login(email_address, email_password)
         smtp.send_message(message)
+
+
+def send_reset_email(user):
+    token = user.get_reset_token()
+
+    # working
+    gmail_user = 'FeedingNewcastle@gmail.com'
+    gmail_password = 'Pea5NudeCure'
+
+    sent_from = gmail_user
+    to = [user.email]
+    subject = 'Password Reset Request'
+    body = f''' To reset your password, click the following link:
+{ url_for('users.reset_token', token=token, _external=True) }
+    
+If this request was not made by you please email us: @feedingnewcastle@gmail.com.
+'''
+    email_text = """\
+    From: %s
+    To: %s
+    Subject: %s
+
+    %s
+    """ % (sent_from, ", ".join(to), subject, body)
+
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server.ehlo()
+    server.login(gmail_user, gmail_password)
+    server.sendmail(sent_from, to, email_text)
+    server.close()
+
+
