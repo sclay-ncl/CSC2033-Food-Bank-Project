1364db2dd703b3544c41d42429d4ca9dedb6adc7
sol <s.clay2@newcastle.ac.uk>
2022-01-10 19:13:15 +0000

    Change to formfield

--- food_banks/forms.py
+++ food_banks/forms.py
@@ -91,7 +91,7 @@ class CategoryBoundaryForm(Form):
 class StockQuantityForm(FlaskForm):
     """Form combing ItemStockForms used to update the quantity of stock across many items"""
     item_forms = FieldList(FormField(ItemStockForm))  # TODO see how this renders in html with front end team
-    category_boundary_form = CategoryBoundaryForm()
+    category_boundary_form = FormField(CategoryBoundaryForm)
     submit = SubmitField()
 
 class StockManagementOptionForm(FlaskForm):
